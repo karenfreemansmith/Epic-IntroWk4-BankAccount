32b99cd96fa6bbda7954c97a9b639af3a4ccb532
Ethan Law and Karen Freeman-Smith <ethan@>
2016-08-23 15:58:19 -0700

    add gamification to encourage savings

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,13 @@ $(document).ready(function(){
     var inputInitialDeposit = parseFloat($("#initialDeposit").val());
     newAccount = new Account(inputName, inputInitialDeposit);
     $(".balance").show();
-    $("#holder").text(newAccount.holder);
-    $("#balance").text("$" + newAccount.balance);
+    $(".transactions").show();
+    $(".registration").hide();
+    showBalance();
   });
   $("form#funds").submit(function(event){
     event.preventDefault();
     var inputAmount = 0;
-
 
     if ($("#deposit").val()>0) {
       inputAmount=parseFloat($("#deposit").val());
@@ -37,10 +37,28 @@ $(document).ready(function(){
     } else {
         alert("You can only deposit money in this account, you may want to consider a safety deposit box.");
       }
-      $(".balance").show();
-      $("#holder").text(newAccount.holder);
-      $("#balance").text("$" + newAccount.balance);
+      // $(".balance").show();
+      showBalance();
       $("#deposit").val("");
       $("#withdrawal").val("");
   });
 });
+
+function showBalance() {
+  $("#holder").text(newAccount.holder);
+  $("#balance").text("$" + newAccount.balance);
+  if (newAccount.balance > 1000000) {
+    $("#photo").html("<img src='img/jet.jpg'>");
+  } else if (newAccount.balance > 100000) {
+    $("#photo").html("<img src='img/leo.jpg'>");
+  } else if (newAccount.balance > 10000) {
+    $("#photo").html("<img src='img/spa.jpg'>");
+  } else if(newAccount.balance>1000) {
+    $("#photo").html("<img src='img/middle-class.jpg'>");
+  } else if(newAccount.balance>0) {
+    $("#photo").html("<img src='img/i-owe.jpg'>");
+  } else {
+    $("#photo").html("<img src='img/guy.jpg'>");
+  }
+
+}
